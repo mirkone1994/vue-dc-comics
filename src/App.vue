04e22760728c11f57116af6710594a27caa514df
mirkone1994 <mirkochiappori@gmail.com>
2021-09-06 15:09:08 +0200

    dist folder

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
+    <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header.vue"
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: auto;
+    margin: auto;
+    box-sizing: border-box;
+  }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="@/assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+
+}
+</script>
+
+<style lang="scss">
+    
+    .container {
+        max-width: 1200px;
+        height: 200px;
+    }
+    .row {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
