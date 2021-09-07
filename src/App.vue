71d4414a89e61787fa68486cf15cb360beec44a3
mirkone1994 <mirkochiappori@gmail.com>
2021-09-07 11:57:29 +0200

    links section done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header/>
+    <Links/>
     <Main/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue"
+import Links from "./components/Links.vue"
 import Main from "./components/Main.vue"
 export default {
   name: 'App',
   components: {
     Header,
+    Links,
     Main,
   }
 }
---
+++ src/components/Links.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="section">
+    <div class="container">
+      <div class="row">
+          <div class="col">
+              <img src="@/assets/img/buy-comics-digital-comics.png" alt="">
+              <span>DIGITAL COMICS</span>
+          </div>
+          <div class="col">
+              <img src="@/assets/img/buy-comics-shop-locator.png" alt="">
+              <span>DC MERCHANDISE</span>
+          </div>
+          <div class="col">
+              <img src="@/assets/img/buy-comics-subscriptions.png" alt="">
+              <span>SUBSCRIPTION</span>
+          </div>
+          <div class="col">
+              <img src="@/assets/img/buy-comics-digital-comics.png" alt="">
+              <span>COMIC SHOP LOCATOR</span>
+          </div>
+          <div class="col">
+              <img src="@/assets/img/buy-dc-power-visa.svg" alt="">
+              <span>DC POWER VISA</span>
+          </div>
+      </div>
+  </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: "Links",
+
+}
+</script>
+
+<style scoped lang="scss">
+    .section {
+        background-color: #0282f9;
+    }
+    .container {
+        max-width: 1400px;
+        height: 200px;
+    }
+    .row {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        height: 100%;
+    }
+    img {
+        height: 50px;
+    }
+    span {
+        vertical-align: center;
+        color: #fff;
+        padding-left: 10px;
+    }
+
+</style>
